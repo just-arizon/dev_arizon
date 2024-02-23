0dad52ff324e27ef198978bb682d47e6cb4c27f0
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-02-24 00:52:06 +0100

    update swiper component

--- src/assets/components/navigation.jsx
+++ src/assets/components/navigation.jsx
@@ -5,7 +5,7 @@ import Logo from "../images/Group 4.svg"
 
 function Navigation() {
   return (
-    <div className='fixed bottom-0 bg-[#] w-full py-2 px-2 flex justify-between'>
+    <div className='fixed bottom-0 bg-[#252525] w-full py-2 px-2 flex justify-between'>
       {/* Logo */}
       <div className="logo flex items-center">
         <img src={Logo} alt="logo" />
@@ -17,21 +17,21 @@ function Navigation() {
              <li className='list__item'>
               <button className='bg-transparent'>
                 <a href='https://github.com/just-arizon' className='nav-icon'>
-                <Icon icon="line-md:github-loop" width="1.6em" height="1.6em" color='#252525'/>
+                <Icon icon="line-md:github-loop" width="1.6em" height="1.6em" color='#fff'/>
                 </a>
               </button>
               </li>
              <li className='list__item'>
               <button className='bg-transparent'>
                 <a href='https://www.linkedin.com/in/arinze-calistus/' className='nav-icon'>
-                             <Icon icon="line-md:linkedin" width="1.4em" height="1.6em" color='#252525'/>
+                             <Icon icon="line-md:linkedin" width="1.4em" height="1.6em" color='#fff'/>
                 </a>
               </button>
               </li>
              <li className='list__item'>
               <button className='bg-transparent'>
                 <a href='https://twitter.com/just_Arizon' className='nav-icon'>
-                             <Icon icon="line-md:twitter" width="1.4em" height="1.6em" color='#252525'/>
+                             <Icon icon="line-md:twitter" width="1.4em" height="1.6em" color='#fff'/>
                 </a>
               </button>
               </li>
@@ -41,7 +41,7 @@ function Navigation() {
       {/* Hamburger */}
       <div className="hamburger flex items-center">
       <Icon icon="solar:hamburger-menu-broken" className="hamburger__icon"
-      width="1.6em" height="1.4em" color='#252525'/>
+      width="1.6em" height="1.4em" color='#fff'/>
       </div>
     </div>
   )
--- src/assets/components/swiper.jsx
+++ src/assets/components/swiper.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import '../styles/swiper.css'
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "../styles/swiper.css";
+
 // import {Button} from "@nextui-org/Button";
 
-
-
 // import required modules
-import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
 
 export default function App() {
   return (
     <>
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         autoplay={{
           delay: 2000,
           disableOnInteraction: false,
         }}
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         loop={true}
         coverflowEffect={{
           rotate: 0,
@@ -35,148 +34,119 @@ export default function App() {
           slideShadows: true,
         }}
         pagination={false}
-        modules={[Autoplay,EffectCoverflow, Pagination]}
+        modules={[Autoplay, EffectCoverflow, Pagination]}
         classNameNameName="mySwiper second-step"
         data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="3000"
-     data-aos-offset="0"
-     
+        data-aos-easing="ease-in-back"
+        data-aos-delay="3000"
+        data-aos-offset="0"
       >
         <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Instagram Clone</div>
-    <button className=''>Visit</button>    
-  </div>
-</div>
+          <div className="card">
+            <div className="tools">
+              <div className="circle">
+                <span className="red box"></span>
+              </div>
+              <div className="circle">
+                <span className="yellow box"></span>
+              </div>
+              <div className="circle">
+                <span className="green box"></span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="card__content">
+                <div className="card_title font-extrabold">Instagram Clone</div>
+                <button className="text-sm btn-style ">Visit</button>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Instagram Clone</div>
-    <button className=''>Visit</button>
-  </div>
-</div>
+          <div className="card">
+            <div className="tools">
+              <div className="circle">
+                <span className="red box"></span>
+              </div>
+              <div className="circle">
+                <span className="yellow box"></span>
+              </div>
+              <div className="circle">
+                <span className="green box"></span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="card__content">
+                <div className="card_title font-extrabold">Instagram Clone</div>
+                <button className="text-sm btn-style ">Visit</button>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Twitter Clone</div>
-    <button className='text-gray-200'>Visit</button>
-  </div>
-</div>
+          <div className="card">
+            <div className="tools">
+              <div className="circle">
+                <span className="red box"></span>
+              </div>
+              <div className="circle">
+                <span className="yellow box"></span>
+              </div>
+              <div className="circle">
+                <span className="green box"></span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="card__content">
+                <div className="card_title font-extrabold">Instagram Clone</div>
+                <button className="text-sm btn-style ">Visit</button>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Twitter Clone</div>
-    <button className=''>Visit</button>
-  </div>
-</div>
+          <div className="card">
+            <div className="tools">
+              <div className="circle">
+                <span className="red box"></span>
+              </div>
+              <div className="circle">
+                <span className="yellow box"></span>
+              </div>
+              <div className="circle">
+                <span className="green box"></span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="card__content">
+                <div className=" font-extrabold card_title">Instagram Clone</div>
+                <button className="text-sm btn-style ">Visit</button>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Verify Mall</div>
-    <button className=''>Visit</button>
-  </div>
-</div>
+          <div className="card">
+            <div className="tools">
+              <div className="circle">
+                <span className="red box"></span>
+              </div>
+              <div className="circle">
+                <span className="yellow box"></span>
+              </div>
+              <div className="circle">
+                <span className="green box"></span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="card__content">
+                <div className="card_title font-extrabold">Instagram Clone</div>
+                <button className="text-sm btn-style ">Visit</button>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Omni Gpt</div>
-    <button className=''>Visit</button>
-  </div>
-</div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="card">
-  <div className="tools">
-    <div className="circle">
-      <span className="red box"></span>
-    </div>
-    <div className="circle">
-      <span className="yellow box"></span>
-    </div>
-    <div className="circle">
-      <span className="green box"></span>
-    </div>
-  </div>
-  <div className="card__content">
-    <div>Omni Gpt</div>
-    <button className=''>Visit</button>
-  </div>
-</div>
-        </SwiperSlide>
-              </Swiper>
+      </Swiper>
     </>
   );
 }
