097ec25d0e5903f398e083fa7ffb5909e1f28c14
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-02-24 17:51:38 +0100

    add experience content

--- src/assets/components/experience.jsx
+++ src/assets/components/experience.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const experience = () => {
   return (
-    <div className='flex justify-center my-12'>
-        <div>
-            <span className="text-[#231e23] text-2xl">Experience</span>
+    <div className='my-12'>
+        <div className='flex justify-center'>
+            <span className="text-[#231e23] text-3xl">Experience</span>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 flex flex-wrap my-10 lg:px-12 p-3'>
+        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
+        <span className=" text-[#00d8c9] text-xl">Build Generation</span>
+        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
+        </div>
+        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
+        <span className=" text-[#00d8c9] text-xl">Summer of Bitcoin</span>
+        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
+        </div>
+        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
+        <span className=" text-[#00d8c9] text-xl">Summer of Bitcoin</span>
+        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
+        </div>
         </div>
     </div>
   );
