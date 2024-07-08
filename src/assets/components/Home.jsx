530ac8b3944c5368e5ae6e41671d3510898563b5
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-07-08 23:56:01 +0100

    Add object props to components

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -6,13 +6,55 @@ import Experience from './experience';
 import Project from './project';
 import Footer from './footer';
 function Home() {
+  const experience = [
+    {
+      id: 1,
+      title: 'Build Generation',
+      date: 'October 2023 - Present',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+    {
+      id: 2,
+      title: 'Summer of Bitcoin',
+      date: 'October 2023 - June 2024',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+    {
+      id: 3,
+      title: 'Outreachy Internship',
+      date: 'March 2024 - August 2024',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+
+  ]
+  const projects = [
+    {
+      id: 1,
+      title: 'Build Generation',
+      date: 'October 2023 - Present',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+    {
+      id: 2,
+      title: 'Summer of Bitcoin',
+      date: 'October 2023 - June 2024',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+    {
+      id: 3,
+      title: 'Outreachy Internship',
+      date: 'March 2024 - August 2024',
+      details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?'
+    },
+
+  ]
   return (
     <div id='home'>
        <Profile />
        <Swiper />
      <Skillset />
-     <Experience />
-     <Project />
+     <Experience experiences={experience}/>
+     <Project projects={projects}/>
      <Footer />
 
     </div>
--- src/assets/components/experience.jsx
+++ src/assets/components/experience.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React from "react";
 import "../styles/experience.css";
 
-const experience = () => {
+const experience = ({ experiences }) => {
+  const renderedList = experiences.map((experience) => {
+    return (
+        <div
+          className="dash__profile rounded p-5 w-11/12"
+          data-speed=""
+          data-aos=""
+          key={experience.id}
+        >
+          <span className=" text-[#00d8c9] text-xl">{experience.title}</span>
+          <div>
+            <span className="text-[#00d8c9] font-normal text-sm">{experience.date}</span>
+          </div>
+          <p className="py-5">{experience.details}</p>
+        </div>
+ 
+    );
+  });
   return (
-    <div className='mt-12' id='experience' >
-        <div className='flex justify-center'>
-            <div className="text-[#00d8c9] text-3xl font-extrabold ">Experience</div>
+    <div className="mt-12" id="experience">
+      <div className="flex justify-center">
+        <div className="text-[#00d8c9] text-3xl font-extrabold ">
+          Experience
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 flex flex-wrap mt-5 lg:px-12 p-3'>
-        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-        <span className=" text-[#00d8c9] text-xl">Build Generation</span>
-        <div><span className='text-[#00d8c9] font-normal text-sm'>October 2023 - Present</span></div>
-        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-        </div>
-        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-        <span className=" text-[#00d8c9] text-xl">Summer of Bitcoin</span>
-        <div><span className='text-[#00d8c9] font-normal text-sm'>October 2023 - June 2024</span></div>
-        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-        </div>
-        <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-        <span className=" text-[#00d8c9] text-xl">Outreachy Internship</span>
-        <div><span className='text-[#00d8c9] font-normal text-sm'>March 2024 - August 2024</span></div>
-        <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-        </div>
-        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 flex flex-wrap mt-5 lg:px-12 p-3">{renderedList}</div>
     </div>
   );
-}
+};
 
 export default experience;
