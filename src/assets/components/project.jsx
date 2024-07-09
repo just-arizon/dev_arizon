0799b436b1cf12573d44c9a95419611150dd0f63
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-07-09 02:38:44 +0100

    project update

--- src/assets/components/project.jsx
+++ src/assets/components/project.jsx
@@ -1,30 +1,55 @@
-import React from 'react';
-
-const project = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+const project = ({ projects }) => {
+  const renderedList = projects.map((project) => {
+    return (
+      <div
+        className="dash__profile rounded p-5 w-11/12"
+        data-speed=""
+        data-aos=""
+        key={project.id}
+      >
+        <span className=" text-[#00d8c9] text-xl">{project.title}</span>
+        <div>
+          <span className=" flex gap-3 mt-3">
+            {project.languages.map((language, index) => (
+              <span
+                key={index}
+                className="bg-transparent border-2 border-[#00d8c9] py-1 px-4 rounded-lg font-thin text-xs"
+              >
+                {language}
+              </span>
+            ))}
+          </span>
+        </div>
+        <p className="py-5">{project.details}</p>
+        <div className="flex gap-3">
+          <Link
+            to={project.preview}
+            className="bg-[#00d8c9] py-2 px-5 rounded-md"
+          >
+            Preview
+          </Link>
+          <Link
+            to={project.code}
+            className="bg-[#00d8c9] py-2 px-5 rounded-md"
+          >
+            Code
+          </Link>
+        </div>
+      </div>
+    );
+  });
   return (
-    <div className='mt-12' id='project'>
-    <div className='flex justify-center'>
-        <div className="text-[#252525] text-3xl font-extrabold ">Projects</div>
+    <div className="mt-12" id="experience">
+      <div className="flex justify-center">
+        <div className="text-[#00d8c9] text-3xl font-extrabold ">Projects</div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 flex flex-wrap mt-5 lg:px-12 p-3">
+        {renderedList}
+      </div>
     </div>
-    <div className='grid grid-cols-1 md:grid-cols-2 flex flex-wrap mt-5 lg:px-12 p-3'>
-    <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-    <span className=" text-[#00d8c9] text-xl">Build Generation</span>
-    <div><span className='text-[#00d8c9] font-normal text-sm'>October 2023 - Present</span></div>
-    <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-    </div>
-    <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-    <span className=" text-[#00d8c9] text-xl">Summer of Bitcoin</span>
-    <div><span className='text-[#00d8c9] font-normal text-sm'>October 2023 - June 2024</span></div>
-    <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-    </div>
-    <div className="dash__profile rounded p-5 w-11/12" data-speed="" data-aos="">
-    <span className=" text-[#00d8c9] text-xl">Outreachy Internship</span>
-    <div><span className='text-[#00d8c9] font-normal text-sm'>March 2024 - August 2024</span></div>
-    <p className='py-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, nesciunt reprehenderit voluptatibus culpa deserunt laudantium nulla ea alias blanditiis quos nihil aperiam, similique animi soluta fugit vel harum perspiciatis assumenda minima quam aliquid a eum possimus sit. Facilis, vero fuga incidunt voluptatem modi enim error eos mollitia ullam, odio quae?</p>
-    </div>
-    </div>
-</div>
   );
-}
+};
 
 export default project;
