2ebfb4686a2c623d911dc59741e471351df4b0b6
Arinze Chinweuba <calistusarinzechinweuba@gmail.com>
2025-09-09 23:59:40 +0100

    more sparkle commit

--- src/layouts/hero.jsx
+++ src/layouts/hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { Github, Linkedin, FileDown } from "lucide-react";
+import { Github, Linkedin, FileDown, Twitter,  } from "lucide-react";
 import { motion } from "framer-motion";
 import { HashLink } from "react-router-hash-link";
 import { ReactTyped } from "react-typed";
@@ -48,7 +48,7 @@ const Hero = () => {
                  <ReactTyped
                 strings={[
                   "Developer",
-                  "Software Engineer",
+                  "Engineer",
                   "Tech Enthusiast",
                 ]}
                 typeSpeed={60}
@@ -103,6 +103,20 @@ const Hero = () => {
               <span className="sr-only">LinkedIn</span>
             </Link>
             <Link
+              to="https://x.com/just_Arizon"
+              className="text-gray-600 hover:text-[#c084fcd9] transition-colors"
+            >
+              <Twitter className="h-5 w-5" />
+              <span className="sr-only">Twitter</span>
+            </Link>
+            <Link
+              to="https://www.youtube.com/@dev_arizon"
+              className="text-gray-600 hover:text-[#c084fcd9] transition-colors"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-youtube-icon lucide-youtube"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/></svg>
+              <span className="sr-only">YouTube</span>
+            </Link>
+            <Link
               to="#"
               className="text-gray-600 hover:text-[#c084fcd9] transition-colors"
             >
@@ -121,13 +135,13 @@ const Hero = () => {
             transition={{ duration: 0.8, ease: "easeOut" }}
             className="flex justify-center"
           >
-            <span className="bg-slate-200 relative left-5 z-30 -top-10">
+            <span className="relative left-5 z-30 -top-10">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="4em"
                 height="4em"
                 viewBox="0 0 24 24"
-                className=" absolute"
+                className="absolute"
               >
                 <g fill="none">
                   <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
